23e72f5b2727df27e31d6614f978af54d59d2ee3
Yury Filimonov <yuriy.filimonov@gmail.com>
2020-01-31 10:45:48 +0300

    Source code Lesson6

---
+++ src/Lesson6/PetAnimal.java
@@ -0,0 +1,11 @@
+package Lesson6;
+
+public abstract class PetAnimal extends Animal  {
+    public PetAnimal(String name, String color, int age) {
+        super(name, color, age);
+    }
+
+    public void test(int a) {
+
+    };
+}
