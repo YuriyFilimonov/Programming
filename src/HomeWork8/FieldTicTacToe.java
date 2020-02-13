da6fdcfd0e3469452009443e796ceaac271e0db4
Yury Filimonov <yuriy.filimonov@gmail.com>
2020-02-13 10:20:50 +0300

    Lesson 8. Продолжение. Use Panel. Draw doesn't work.

--- src/HomeWork8/FieldTicTacToe.java
+++ src/HomeWork8/FieldTicTacToe.java
@@ -5,11 +5,14 @@ import java.awt.*;
 import java.util.Random;
 
 public class FieldTicTacToe extends JFrame {
+    final int CELLS = 9;
+    final String X = "x";
+    final String O = "0";
     Random random = new Random();
     ButtonX buttonX;
     ButtonO buttonO;
     JPanel jPanelButton, jPanelText;
-    int zero;
+    int zero, counter;
 
     public FieldTicTacToe() {
 
@@ -25,14 +28,15 @@ public class FieldTicTacToe extends JFrame {
         add(jPanelButton, BorderLayout.CENTER);
         add(jPanelText, BorderLayout.SOUTH);
 
-        JButton[] jb = new JButton[9];
+        JButton[] jb = new JButton[CELLS];
         jPanelButton.setLayout(new GridLayout(3, 3));
         for (int i = 0; i < jb.length; i++) {
             jb[i] = new JButton("•");
             jPanelButton.add(jb[i]);
         }
 
-        JTextField jTextField = new JTextField("Turn of Player");
+        JTextField jTextField = new JTextField();
+        counterCells(jTextField);
 //        setLayout(new BorderLayout());
         jPanelText.add(jTextField);
         setVisible(true);
@@ -40,24 +44,59 @@ public class FieldTicTacToe extends JFrame {
 //        Обработаем событие нажатие левой кнопкой мыши на кнопку
         for (JButton jButton : jb) {
             jButton.addActionListener(e -> {
-                jButton.setText(null);
+                jButton.setText(X);
+                counterCells(jTextField);
                 buttonX = new ButtonX();
                 jButton.add(buttonX);
-                try {
-                    while (true) {
-                        zero = random.nextInt(jb.length);
-                        if (jb[zero].getText().equals("•")){
-                            jb[zero].setText(null);
-                            buttonO = new ButtonO();
-                            jb[zero].add(buttonO);
-                            return ;
-                        }
-                    }
-                } catch (NullPointerException e1) {
-                    System.out.println("Ошибка");
+
+                if (checkWin(jb, O)) {
+                    jTextField.setText("AI Win");
+                    return;
+//                    System.exit(0); Приложение закрывается
                 }
-           });
+                else if (checkWin(jb, X)) {
+                    jTextField.setText("Player Win");
+                    return;
+//                    System.exit(0); Приложение закрывается
+                }
+
+                ++counter;
+                if(counter == 9){
+                    jTextField.setText("Draw, cells ended.");
+                }
+
+                while (true) {
+                    zero = random.nextInt(jb.length);
+                    if (!(jb[zero].getText().equals(X)) && !(jb[zero].getText().equals(O))) {
+                        jb[zero].setText(O);
+                        counter++;
+                        counterCells(jTextField);
+                        buttonO = new ButtonO();
+                        jb[zero].add(buttonO);
+                        return;
+                    }
+                }
+            });
         }
+    }
+
+    private void counterCells(JTextField jTextField) {
+        jTextField.setText("Осталось " + (CELLS - counter) + " клеток");
+    }
+
+    private boolean checkWin(JButton[] jb, String s) {
+        for (int i = 0; i < CELLS; i += 3) {
+            if (jb[i].getText().equals(s) && jb[i + 1].getText().equals(s) && jb[i + 2].getText().equals(s)) {
+                return true;
+            }
+        }
+        for (int i = 0; i < CELLS / 3; i++) {
+            if (jb[i].getText().equals(s) && jb[i + 3].getText().equals(s) && jb[i + 6].getText().equals(s)) {
+                return true;
+            }
+        }
+        return (jb[0].getText().equals(s) && jb[4].getText().equals(s) && jb[8].getText().equals(s)) ||
+                (jb[2].getText().equals(s) && jb[4].getText().equals(s) && jb[6].getText().equals(s));
     }
 
     public static void main(String[] args) {
